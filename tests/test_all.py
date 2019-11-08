46892e37909e3b99418984d5a03cf39e1e640c84
jorana <jorana@nikhef.nl>
2019-11-08 16:43:39 +0100

    update tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,4 +1,4 @@
 import os
-
-os.system("python ../run_dddm.py -nwalkers 5 -nsteps 7")
-os.system("python ../run_dddm_nestle.py -live 10 -tol 0.9")
+os.system("pwd")
+os.system("python run_dddm.py -nwalkers 5 -nsteps 7")
+os.system("python run_dddm_nestle.py -live 10 -tol 0.9")
