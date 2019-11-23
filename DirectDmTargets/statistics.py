876eaa8d710226b0c74865b2f78894291fcd4c40
jorana <jorana@nikhef.nl>
2019-11-23 23:50:12 +0100

    ran summary for longer

--- DirectDmTargets/statistics.py
+++ DirectDmTargets/statistics.py
@@ -44,6 +44,17 @@ def get_priors(priors_from="Evans_2019"):
                   'v_esc': {'range': [379, 709], 'prior_type': 'gauss',
                             'mean': 528, 'std': 24.5},
                   'k': {'range': [0.5, 3.5], 'prior_type': 'flat'}}
+    elif priors_from == "realistic":
+        priors = {'log_mass': {'range': [0.01, 4], 'prior_type': 'flat'},
+                  'log_cross_section': {'range': [-49, -44],
+                                        'prior_type': 'flat'},
+                  'density': {'range': [0.001, 0.9], 'prior_type': 'gauss',
+                              'mean': 0.55, 'std': 0.1},
+                  'v_0': {'range': [80, 380], 'prior_type': 'gauss',
+                          'mean': 233, 'std': 3},
+                  'v_esc': {'range': [379, 709], 'prior_type': 'gauss',
+                            'mean': 528, 'std': 24.5},
+                  'k': {'range': [0.5, 3.5], 'prior_type': 'flat'}}
     else:
         raise NotImplementedError(f"Taking priors from {priors_from} is not "
                                   f"implemented")
