8f6379b53660a314e68c9b4b99f8fb15a1b4c4a8
Joran Angevaare <jorana@nikhef.nl>
2020-04-21 09:25:03 +0200

    Remove disabled code
    Remove code that was commented out

--- DirectDmTargets/halo.py
+++ DirectDmTargets/halo.py
@@ -240,17 +240,7 @@ class VerneSHM:
             self.v_0_nodim, self.v_esc_nodim, self.rho_dm_nodim,
             self.log_cross_section, self.log_mass)
 
-        # TODO
-        #  Temporary check that these parameters are in a reasonable range.
-        #     assert_str = "double check these parameters"
-        #     for i, param in enumerate([self.v_esc_nodim, self.v_0_nodim, self.rho_dm_nodim]):
-        #         ref_val = [230, 544, 0.3][i]
-        #         # values should be comparable to the reference value
-        #         assert (abs((ref_val - param) / ref_val) < 5 and
-        #                 abs((ref_val - param) / param) < 5), assert_str + f'\nparameter is {param} vs. ref val of {ref_val}'
         self.itp_func = None
-    # TODO
-    #  def __str__(self):
 
     def __str__(self):
         return 'shielded_shm'
@@ -262,7 +252,6 @@ class VerneSHM:
         '''
 
         # set up folders and names
-#         folder = get_verne_folder() + 'results/veldists/'
         file_folder = context['verne_files']
         software_folder = context['verne_folder']                         
         # TODO
@@ -271,7 +260,6 @@ class VerneSHM:
         low_n_gamma = False
         if low_n_gamma:
             self.fname = 'tmp_' + self.fname
-#         file_name = file_folder + self.fname + '_avg' + '.csv'
         file_name = file_folder + self.fname + '_avg' + '.csv'
         check_folder_for_file(file_folder + self.fname, verbose=0)
 
@@ -283,7 +271,7 @@ class VerneSHM:
 
         if not exist_csv:
             pyfile = '/src/CalcVelDist.py'
-            file_name = abs_file_name # file_folder + self.fname + '_' + host + '_avg' + '.csv'
+            file_name = abs_file_name 
             args = f'-m_x {10. ** self.log_mass} ' \
                    f'-sigma_p {10. ** self.log_cross_section} ' \
                    f'-loc {self.location} ' \
@@ -299,10 +287,6 @@ class VerneSHM:
             print(f'No spectrum found at:\n{file_name}\nGenerating spectrum, '
                   f'this can take a minute. Execute:\n{cmd}')
             os.system(cmd)
-        # elif is_str_in_list(csv_key, files_in_folder):
-        #     print(f'Using {str_in_list(csv_key, files_in_folder)} since it has {csv_key}')
-        #     file_name = csv_path + str_in_list(csv_key, files_in_folder)
-        #     print(f'Using {file_name} for the velocity distribution')
         else:
             print(f'Using {abs_file_name} for the velocity distribution')
 
@@ -328,135 +312,3 @@ class VerneSHM:
         if self.itp_func == None:
             self.load_f()
         return self.itp_func(v, t)
-
-
-
-
-# class ContinuousVerneSHM:
-#     """
-#         class used to pass a halo model to the rate computation based on the
-#         earth shielding effect as calculated by Verne
-#         must contain:
-#         :param v_esc -- escape velocity (multiplied by units)
-#         :param rho_dm -- density in mass/volume of dark matter at the Earth (multiplied by units)
-#         The standard halo model also allows variation of v_0
-#         :param v_0 -- v0 of the velocity distribution (multiplied by units)
-#         :function velocity_dist -- function taking v,t giving normalised
-#         velocity distribution in earth rest-frame.
-#        """
-
-#     def __init__(self, v_0=None, v_esc=None, rho_dm=None,
-#                  log_cross_section=None, log_mass=None, location=None):
-#         # This may seem somewhat counter-intuitive. But we want to use similar
-#         # input as to SHM (see above) to that end, we here divide the input by
-#         # the respective units
-#         self.v_0_nodim = 230 if v_0 is None else v_0 / (nu.km / nu.s)
-#         self.v_esc_nodim = 544 if v_esc is None else v_esc / (nu.km / nu.s)
-#         self.rho_dm_nodim = 0.3 if rho_dm is None else rho_dm / (nu.GeV / nu.c0 ** 2 / nu.cm ** 3)
-
-#         # Here we keep the units dimentionfull as these paramters are requested
-#         # by wimprates and therefore must have dimensions
-#         self.v_0 = self.v_0_nodim * nu.km / nu.s
-#         self.v_esc = self.v_esc_nodim * nu.km / nu.s
-#         self.rho_dm = self.rho_dm_nodim * nu.GeV / nu.c0 ** 2 / nu.cm ** 3
-
-#         # in contrast to the SHM, the earth shielding does need the mass and
-#         # cross-section to calculate the rates.
-#         self.log_cross_section = -35 if log_cross_section is None else log_cross_section
-#         self.log_mass = 0 if log_mass is None else log_mass
-#         self.location = "XENON" if location is None else location
-
-#         # # Combine the parameters into a single naming convention. This is were
-#         # # we will save/read the velocity distributuion (from).
-#         # self.fname = 'f_params/loc_%s/v0_%i/vesc_%i/rho_%.2f/sig_%.1f_mx_%.2f' % (
-#         #     self.location,
-#         #     self.v_0_nodim, self.v_esc_nodim, self.rho_dm_nodim,
-#         #     self.log_cross_section, self.log_mass)
-#         #
-#         # # TODO
-#         # #  Temporary check that these parameters are in a reasonable range.
-#         assert_str = "double check these parameters"
-#         for i, param in enumerate([self.v_esc_nodim, self.v_0_nodim, self.rho_dm_nodim]):
-#             ref_val = [230, 544, 0.3][i]
-#             # values should be comparable to the reference value
-#             assert (abs((ref_val - param) / ref_val) < 5 and
-#                     abs((ref_val - param) / param) < 5), assert_str + f'\nparameter is {param} vs. ref val of {ref_val}'
-#         # self.itp_func = None
-
-#     # def load_f(self):
-#     #     '''
-#     #     load the velocity distribution. If there is no velocity distribution shaved, load one.
-#     #     :return:
-#     #     '''
-#     #
-#     #     # set up folders and names
-#     #     folder = get_verne_folder() + 'results/veldists/'
-#     #     # TODO
-#     #     #  This is a statement to get the data faster, i.e. take a short-cut (we
-#     #     #  only compute 2 angles and take the average)
-#     #     low_n_gamma = True
-#     #     if low_n_gamma:
-#     #         self.fname = 'tmp_' + self.fname
-#     #     file_name = folder + self.fname + '_avg' + '.csv'
-#     #     check_folder_for_file(folder + self.fname)
-#     #
-#     #     # if no data available here, we need to make it
-#     #     if not os.path.exists(file_name):
-#     #         pyfile = '/src/CalcVelDist.py'
-#     #         args = f'-m_x {10 ** self.log_mass} ' \
-#     #                f'-sigma_p {10 ** self.log_cross_section} ' \
-#     #                f'-loc {self.location} ' \
-#     #                f'-path "{get_verne_folder()}/src/" ' \
-#     #                f'-v_0 {self.v_0_nodim} ' \
-#     #                f'-v_esc {self.v_esc_nodim} ' \
-#     #                f'-save_as "{file_name}" '
-#     #         if low_n_gamma:
-#     #             # Set N_gamma low for faster computation (only two angles)
-#     #             args += f' -n_gamma 2'
-#     #
-#     #         cmd = f'python "{get_verne_folder()}"{pyfile} {args}'
-#     #         print(f'No spectrum found at:\n{file_name}\nGenerating spectrum, '
-#     #               f'this can take a minute. Execute:\n{cmd}')
-#     #         os.system(cmd)
-#     #     else:
-#     #         print(f'Using {file_name} for the velocity distribution')
-#     #
-#     #     # Alright now load the data and interpolate that. This is the output that wimprates need
-#     #     df = pd.read_csv(file_name)
-#     #     x, y = df.keys()
-#     #     interpolation = interp1d(df[x] * (nu.km / nu.s), df[y] * (nu.s / nu.km), bounds_error=False, fill_value=0)
-#     #
-#     #     # Wimprates needs to have a two-parameter function. However since we
-#     #     # ignore time for now. We make this makeshift transition from a one
-#     #     # parameter function to a two parameter function
-#     #     def velocity_dist(v_, t_):
-#     #         return interpolation(v_)
-#     #
-#     #     self.itp_func = velocity_dist
-
-
-#     def load_dist(self, v):
-#         low_n_gamma = True
-#         distribution_class = CalcVelDist_per_v.CalcVelDist(
-#             m_x=10 ** self.log_mass,
-#             sigma_p=10 ** self.log_cross_section,
-#             path=get_verne_folder()+'/src/',
-#             location=self.location,
-#             v_0=self.v_0_nodim,
-#             v_esc=self.v_esc_nodim,
-#             n_gamma=2 if low_n_gamma else 11)
-
-#         return distribution_class.get_f(v)
-
-#     def velocity_dist(self, v, t):
-#         # in units of per velocity,
-#         # v is in units of velocity
-
-#         def return_velocity_dist(v, t):
-#             # TODO
-#             #  for now, the time is not taken into account, hence, this work
-#             #  around does add a mock-up dependency of the distribution to t.
-#             #  This is an option in verne and can be implemented.
-#             return self.load_dist(v)
-
-#         return return_velocity_dist(v, t)
