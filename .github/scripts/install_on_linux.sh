24f20c8513cfac6f76242a577e46d6993c101003
Joran Angevaare <jorana@nikhef.nl>
2021-01-27 23:36:06 +0100

    other way

--- .github/scripts/install_on_linux.sh
+++ .github/scripts/install_on_linux.sh
@@ -6,19 +6,19 @@ sudo apt-get install -qq openmpi-bin libopenmpi-dev python-mpi4py
 echo "doing pip install"
 echo "cloning dir"
 git clone https://github.com/JohannesBuchner/MultiNest
-cd MultiNest/build
-cmake ..
-make
-echo "made MultiNest"
-echo "doing cd .. ; ls ; pwd ; cd .."
-cd ..
-ls
-pwd
-cd ..
-echo "Setting evn var"
-export LD_LIBRARY_PATH=/home/runner/work/DirectDmTargets/DirectDmTargets/MultiNest/lib
-#mkdir -p MultiNest/build; pushd MultiNest/build; cmake .. && make && popd
-#test -e MultiNest/lib/libmultinest.so
+#cd MultiNest/build
+#cmake ..
+#make
+#echo "made MultiNest"
+#echo "doing cd .. ; ls ; pwd ; cd .."
+#cd ..
+#ls
+#pwd
+#cd ..
+#echo "Setting evn var"
+#export LD_LIBRARY_PATH=/home/runner/work/DirectDmTargets/DirectDmTargets/MultiNest/lib
+mkdir -p MultiNest/build; pushd MultiNest/build; cmake .. && make && popd
+test -e MultiNest/lib/libmultinest.so
 #python setup.py install --user
 #python -c 'import pymultinest'|grep 'LD_LIBRARY_PATH
 pip install pymultinest
