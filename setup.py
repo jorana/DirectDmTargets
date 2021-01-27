c5540d059a19fa42ab2787a66ab8f2ffcc774ab8
Joran Angevaare <jorana@nikhef.nl>
2021-01-27 16:44:06 +0100

    use workflows

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ requirements = open('requirements.txt').read().splitlines()
 setuptools.setup(
     name='DirectDmTargets',
     version='0.4.0',
-    description='Probing the complementarity of several targets used in Direct Detection Experiments for Dark Matter',
+    description='Probing the complementarity of several targets used in '
+                'Direct Detection Experiments for Dark Matter',
     long_description=readme + '\n\n' + history,
     author='Joran Angevaare',
     url='https://github.com/jorana/DD_DM_targets',
-    # license='MIT',
     packages=setuptools.find_packages(),
     setup_requires=['pytest-runner'],
     install_requires=requirements,
@@ -19,6 +19,10 @@ setuptools.setup(
     package_data={'DirectDmTargets': [
         'data/*']},
     tests_require=requirements + ['pytest'],
+    scripts=['scripts/run_combined_multinest',
+             'scripts/run_dddm_emcee',
+             'scripts/run_dddm_multinest',
+             ],
     keywords='todo',
     classifiers=['Intended Audience :: Science/Research',
                  'Development Status :: 3 - Alpha',
--- tests/test_all.py
+++ tests/test_all.py
@@ -5,12 +5,12 @@ print(f'Running on {platform}')
 
 if 'win' in platform:
     def test_emcee_windows():
-        os.system("python run_dddm_emcee.py "
+        os.system("run_dddm_emcee "
                   "-nwalkers 10 "
                   "-nsteps 10")
 
     def test_multinest_windows():
-        os.system("python run_dddm_multinest.py "
+        os.system("run_dddm_multinest "
                   "-nlive 10 "
                   "-tol 0.9 "
                   "-sampler nestle "
@@ -19,32 +19,32 @@ else:
     print("happy travis")
 
     def test_emcee():
-        os.system("python run_dddm_emcee.py "
+        os.system("run_dddm_emcee "
                   "-nwalkers 10 "
                   "-nsteps 10")
 
     def test_multinest_no_shield():
-        os.system("python run_dddm_multinest.py "
+        os.system("run_dddm_multinest "
                   "-nlive 10 "
                   "-tol 0.999 "
                   "-sampler nestle "
                   "-shielding no")
 
     def test_multinest_no_shield_save_interim():
-        os.system("python run_dddm_multinest.py "
+        os.system("run_dddm_multinest "
                   "-nlive 10 "
                   "-tol 0.999 "
                   "-shielding no "
                   "-save_intermediate yes")
 
     def test_multinest_shielding():
-        os.system("python run_dddm_multinest.py "
+        os.system("run_dddm_multinest "
                   "-nlive 10 "
                   "-tol 0.9999 "
                   "-shielding yes")
 
     def test_multinest_multicore():
-        os.system("mpiexec -n 3 python run_dddm_multinest.py "
+        os.system("mpiexec -n 3 run_dddm_multinest "
                   "-nlive 10 "
                   "-tol 0.999 "
                   "-shielding no "
