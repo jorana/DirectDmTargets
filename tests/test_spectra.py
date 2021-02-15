51b830246f369515afca52957086877a933089c7
Joran Angevaare <jorana@nikhef.nl>
2021-02-15 22:05:30 +0100

    reinstate tests

--- tests/test_spectra.py
+++ tests/test_spectra.py
@@ -5,33 +5,32 @@ import numericalunits as nu
 import matplotlib.pyplot as plt
 
 
-# def test_simple_spectrum():
-#     energies = np.linspace(0.01, 20, 50)
-#
-#     # dr/dr
-#     dr = ((nu.keV * (1000 * nu.kg) * nu.year) *
-#           wr.rate_migdal(energies * nu.keV,
-#                          mw=5 * nu.GeV / nu.c0 ** 2,
-#                          sigma_nucleon=1e-35 * nu.cm ** 2))
-#
-#     plt.plot(energies, dr, label="WIMPrates SHM")
-#     dr = ((nu.keV * (1000 * nu.kg) * nu.year) *
-#           wr.rate_migdal(energies * nu.keV,
-#                          mw=0.5 * nu.GeV / nu.c0 ** 2,
-#                          sigma_nucleon=1e-35 * nu.cm ** 2))
-#
-#     plt.plot(energies, dr, label="WIMPrates SHM")
-#
-#     plt.xlabel("Recoil energy [keV]")
-#     plt.ylabel("Rate [events per (keV ton year)]")
-#
-#     plt.xlim(0, energies.max())
-#     plt.yscale("log")
-#
-#     plt.ylim(1e-4, 1e8)
-#     plt.clf()
-#     plt.close()
-#     # dr
+def test_simple_spectrum():
+    energies = np.linspace(0.01, 20, 50)
+
+    # dr/dr
+    dr = ((nu.keV * (1000 * nu.kg) * nu.year) *
+          wr.rate_migdal(energies * nu.keV,
+                         mw=5 * nu.GeV / nu.c0 ** 2,
+                         sigma_nucleon=1e-35 * nu.cm ** 2))
+
+    plt.plot(energies, dr, label="WIMPrates SHM")
+    dr = ((nu.keV * (1000 * nu.kg) * nu.year) *
+          wr.rate_migdal(energies * nu.keV,
+                         mw=0.5 * nu.GeV / nu.c0 ** 2,
+                         sigma_nucleon=1e-35 * nu.cm ** 2))
+
+    plt.plot(energies, dr, label="WIMPrates SHM")
+
+    plt.xlabel("Recoil energy [keV]")
+    plt.ylabel("Rate [events per (keV ton year)]")
+
+    plt.xlim(0, energies.max())
+    plt.yscale("log")
+
+    plt.ylim(1e-4, 1e8)
+    plt.clf()
+    plt.close()
 
 
 def _detector_spectrum_inner(use_SHM):
@@ -47,9 +46,9 @@ def _detector_spectrum_inner(use_SHM):
     events.get_data(poisson=False)
 
 
-# def test_detector_spectrum():
-#     use_SHM = dddm.SHM()
-#     _detector_spectrum_inner(use_SHM)
+def test_detector_spectrum():
+    use_SHM = dddm.SHM()
+    _detector_spectrum_inner(use_SHM)
 
 
 def test_shielded_detector_spectrum():
