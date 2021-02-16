9223717d5abaea62ae424f7def1b882ac0e19ec4
Joran Angevaare <jorana@nikhef.nl>
2021-02-16 16:54:20 +0100

    simplify temp usage

--- DirectDmTargets/context.py
+++ DirectDmTargets/context.py
@@ -8,9 +8,12 @@ from warnings import warn
 
 import DirectDmTargets
 import verne
+import logging
+log = logging.getLogger()
 
 host = getfqdn()
-print(f'Host: {host}')
+log.debug(f'Host: {host}')
+_naive_tmp = '/tmp/'
 
 if 'stbc' in host or 'nikhef' in host:
     context = {'software_dir': '/project/xenon/jorana/software/DD_DM_targets/',
@@ -20,27 +23,25 @@ if 'stbc' in host or 'nikhef' in host:
                'verne_files': '/dcache/xenon/jorana/dddm/verne/'}
     if 'TMPDIR' in os.environ.keys():
         tmp_folder = os.environ['TMPDIR']
-        print(f'found TMPDIR! on {host}')
-    elif os.path.exists('/tmp/'):
-        print("Setting tmp folder to /tmp/")
-        tmp_folder = '/tmp/'
+        log.debug(f'found TMPDIR! on {host}')
+    elif os.path.exists(_naive_tmp):
+        log.debug("Setting tmp folder to /tmp/")
+        tmp_folder = _naive_tmp
         if host == 'stbc-i1.nikhef.nl' or host == 'stbc-i2.nikhef.nl':
             # Fine, we can use the /tmp/ folder
             pass
         else:
             # Not fine, we cannot use the /tmp/ folder on the stoomboot nodes
-            print(f'No tmp folder found on {host}. Environment vars:')
-            for key in os.environ.keys():
-                print(key)
-            assert False
-    assert os.path.exists(
-        tmp_folder), f"Cannot find tmp folder at {tmp_folder}"
+            raise ValueError(f'No tmp folder found on {host}. '
+                             f'Environment vars:\n{os.environ}')
+    if os.path.exists(tmp_folder):
+        raise FileNotFoundError(f"Cannot find tmp folder at {tmp_folder}")
     context['tmp_folder'] = tmp_folder
     for key in context.keys():
         assert os.path.exists(context[key]), f'No folder at {context[key]}'
 else:
     # Generally people will end up here
-    print(f'context.py::\tunknown host {host} be careful here')
+    log.debug(f'context.py::\tunknown host {host} be careful here')
     installation_folder = DirectDmTargets.__path__[0]
     vene_folder = os.path.join(os.path.split(verne.__path__[0])[0], 'results')
     context = {'software_dir': installation_folder,
@@ -52,28 +53,27 @@ else:
                'verne_files': vene_folder,
                }
 
-    if os.path.exists('/tmp/'):
-        print("Setting tmp folder to /tmp/")
-        tmp_folder = '/tmp/'
-    elif 'TMPDIR' in os.environ.keys():
+    if 'TMPDIR' in os.environ:
         tmp_folder = os.environ['TMPDIR']
-        print(f'found TMPDIR! on {host}')
-    elif 'TMP' in os.environ.keys():
+        log.debug(f'found TMPDIR! on {host}')
+    elif 'TMP' in os.environ:
         tmp_folder = os.environ['TMP']
-        print(f'found TMP! on {host}')
+        log.debug(f'found TMP! on {host}')
+    elif os.path.exists(_naive_tmp):
+        log.debug(f"Setting tmp folder to {_naive_tmp}")
+        tmp_folder = _naive_tmp
     assert os.path.exists(tmp_folder), f"No tmp folder at {tmp_folder}"
     context['tmp_folder'] = tmp_folder
     for name in ['results_dir', 'spectra_files']:
-        print(f'context.py::\tlooking for {name} in {context}')
+        log.debug(f'context.py::\tlooking for {name} in {context}')
         if not os.path.exists(context[name]):
             try:
                 os.mkdir(context[name])
             except Exception as e:
-                warn(f'Could not find nor make {context[name]}'
-                     f"Tailor context.py to your needs. Could not initialize "
-                     f"folders correctly because of {e}.")
+                log.warning(
+                    f'Could not find nor make {context[name]}. Tailor '
+                    f'context.py to your needs. Could not initialize folders '
+                    f'correctly because of {e}.')
     for key in context.keys():
         if not os.path.exists(context[key]):
-            warn(f'No folder at {context[key]}')
-
-print(context)
+            log.warning(f'No folder at {context[key]}')
