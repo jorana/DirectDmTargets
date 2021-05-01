a4ef4c691ae228a4161711b452b00ea584b91192
Joran Angevaare <jorana@nikhef.nl>
2021-05-01 16:29:36 +0200

    fix models

--- tests/test_multinest_shielded.py
+++ tests/test_multinest_shielded.py
@@ -12,10 +12,11 @@ def test_nested_simple_multinest_earth_shielding():
         return
     fit_class = dddm.NestedSamplerStatModel('Xe')
     fit_class.config['tol'] = 0.1
-    fit_class.config['nlive'] = 40
+    fit_class.config['nlive'] = 5
     fit_class.config['earth_shielding'] = True
-    fit_class.config['max_iter'] = 10
+    fit_class.config['max_iter'] = 1
     fit_class.config['save_intermediate'] = True
+    fit_class.set_models()
     fit_class.set_nbins()
     fit_class.set_benchmark()
     fit_class.print_before_run()
