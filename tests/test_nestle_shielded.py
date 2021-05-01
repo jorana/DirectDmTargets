0846de034fcbde407fb2b7fa3b759758c1b9828a
Joran Angevaare <jorana@nikhef.nl>
2021-05-01 12:56:02 +0200

    set models before running

--- tests/test_nestle_shielded.py
+++ tests/test_nestle_shielded.py
@@ -12,6 +12,7 @@ def test_nested_simple_nestle_earth_shielding():
     fit_class.config['earth_shielding'] = True
     fit_class.config['save_intermediate'] = True
     log.info(f"Fitting for parameters:\n{fit_class.config['fit_parameters']}")
+    fit_class.set_models()
     fit_class.print_before_run()
     fit_class.run_nestle()
     fit_class.get_summary()
